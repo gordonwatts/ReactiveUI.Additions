ed7c8c6c11735e09b8dc0bee1669d2e7603e2e9e
Gordon Watts <gordon@gordonandpaula.net>
2013-12-31 23:51:42 -0500

    Better version propagation

--- BuildAndPushToMyGet.ps1
+++ BuildAndPushToMyGet.ps1
@@ -3,10 +3,12 @@
 #
 param([parameter(Mandatory = $true)][string] $ApiKey)
 
+$version="1.0.0-alpha02"
+
 set-alias nuget .\.nuget\nuget.exe
 
 ls *.nupkg | % { rm $_ }
 
-nuget pack -symbols ReactiveUI.Additions.nuspec
-nuget push ReactiveUI.Additions.1.0.0-alpha01.nupkg -source https://www.myget.org/F/mvvm/ -ApiKey $ApiKey
-nuget push ReactiveUI.Additions.1.0.0-alpha01.symbols.nupkg -source https://nuget.symbolsource.org/MyGet/mvvm -ApiKey $ApiKey
+nuget pack -symbols -Properties version=$version ReactiveUI.Additions.nuspec
+nuget push ReactiveUI.Additions.$version.nupkg -source https://www.myget.org/F/mvvm/ -ApiKey $ApiKey
+nuget push ReactiveUI.Additions.$version.symbols.nupkg -source https://nuget.symbolsource.org/MyGet/mvvm -ApiKey $ApiKey
